e3d720194c874f7419f954e33e0458f6cce43a2c
Dinomic <hthangnguyen1101@gmail.com>
2025-11-30 15:54:24 +0700

    242

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import solutions.Solution217;
+import solutions.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Solution217 sol = new Solution217();
-        System.out.println(sol.containsDuplicate(new int[]{0,2,3,1}));
+        Solution242 sol = new Solution242();
+        System.out.println(sol.isAnagram("anagram", "nagaram"));
     }
 }
---
+++ src/solutions/Solution242.java
@@ -0,0 +1,31 @@
+package solutions;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Solution242 {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null || s.length() != t.length()) {return false;}
+        
+//        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+//        for (char c : s.toCharArray()) {
+//            map.put(c, map.getOrDefault(c, 0) + 1);;
+//        }
+//        for (char c : t.toCharArray()) {
+//            Integer temp = map.get(c);
+//            if (temp == null || temp == 0) {return false;}
+//            map.put(c, temp - 1);;
+//        }
+//        return true;
+
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        String ssorted = new String(chars);
+
+        chars = t.toCharArray();
+        Arrays.sort(chars);
+        String tsorted = new String(chars);
+
+        return ssorted.equals(tsorted);
+    }
+}
