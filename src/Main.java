bf0343ddc623afada057cb776b85c96c9637ada5
Dinomic <hthangnguyen1101@gmail.com>
2026-01-21 23:22:22 +0700

    Solution1871JumpGameVII

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import solutions.*;
 public class Main {
     public static void main(String[] args) {
         Solution1871JumpGameVII sol = new Solution1871JumpGameVII();
-        System.out.println(sol.canReach("01", 1, 1));
+        System.out.println(sol.canReach("0000000000000000000000000000000000000000000000000000000000000000000000000000000000000", 1, 4));
 //        System.out.println(Arrays.toString(Arrays.stream(sol.topKFrequent(new int[]{3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6}, 10)).toArray()));
     }
 }
